203da61381dd7a2e62f2eeb5a6eabbd508757417
Elías Cabeza de Vaca <elways38@gmail.com>
2026-03-17 14:38:09 -0500

    adding new files

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,7 +1,7 @@
 const pool = require("../database/");
 
 /* ***************************
- * Obtener todos los datos de clasificación
+ * Get all classification data
  * ************************** */
 async function getClassifications() {
   return await pool.query(
@@ -9,4 +9,23 @@ async function getClassifications() {
   );
 }
 
-module.exports = { getClassifications };
+/* ***************************
+ * Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory AS i 
+      JOIN public.classification AS c 
+      ON i.classification_id = c.classification_id 
+      WHERE i.classification_id = $1`,
+      [classification_id],
+    );
+    return data.rows;
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error);
+  }
+}
+
+// AQUÍ ESTABA EL ERROR: Asegúrate de que los nombres coincidan exactamente
+module.exports = { getClassifications, getInventoryByClassificationId };
---
+++ routes/inventoryRoute.js
@@ -0,0 +1,10 @@
+// Recursos Necesarios
+const express = require("express");
+const router = new express.Router();
+const invController = require("../controllers/invController");
+
+// Ruta para construir la vista de inventario por clasificación
+// El ":classificationId" es un parámetro variable
+router.get("/type/:classificationId", invController.buildByClassificationId);
+
+module.exports = router;
