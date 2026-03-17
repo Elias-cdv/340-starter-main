56c122ea90c8c2316e84a4dcb6967393aa96b909
Elías Cabeza de Vaca <elways38@gmail.com>
2026-03-17 11:35:26 -0500

    fix code in inventory-model.js

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,5 +1,8 @@
 const pool = require("../database/");
 
+/* ***************************
+ * Obtener todos los datos de clasificación
+ * ************************** */
 async function getClassifications() {
   return await pool.query(
     "SELECT * FROM public.classification ORDER BY classification_name",
