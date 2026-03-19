2bd227b2e29c1e395982af38e793d583fd70d171
Elías Cabeza de Vaca <elways38@gmail.com>
2026-03-19 16:09:55 -0500

    add inventory route

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,24 +2,11 @@
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
-const utilities = require("../utilities/");
 
 // Ruta para construir la vista de inventario por clasificación
 // El ":classificationId" es un parámetro variable
 router.get("/type/:classificationId", invController.buildByClassificationId);
-
-router.get("/detail/:invId", utilities.handleErrors(invCont.getVehicleDetail));
-
-// Para la lista de carros:
-router.get(
-  "/type/:classificationId",
-  utilities.handleErrors(invController.buildByClassificationId),
-);
-
-// Para el detalle de UN carro:
-router.get(
-  "/detail/:invId",
-  utilities.handleErrors(invController.getVehicleDetail),
-);
+router.get("/detail/:invId", invController.buildDetailView);
+router.get("/trigger-error", invController.triggerError);
 
 module.exports = router;
