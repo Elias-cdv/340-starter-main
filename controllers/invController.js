eab9126467e7e1cfd16ce13e9fd0f77bfa64e8f2
Elías Cabeza de Vaca <elways38@gmail.com>
2026-03-19 14:12:24 -0500

    fix controller

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,11 +1,30 @@
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities/");
+const invCont = {}; // <--- ¡ESTA ES LA LÍNEA QUE TE FALTABA!
+
 /* ***************************
- * Entregar vista de detalle de vehículo
+ * Construir vista de inventario por ID de clasificación
+ * ************************** */
+invCont.buildByClassificationId = async function (req, res, next) {
+  const classification_id = req.params.classificationId;
+  const data = await invModel.getInventoryByClassificationId(classification_id);
+  const grid = await utilities.buildClassificationGrid(data);
+  let nav = await utilities.getNav();
+  const className = data[0].classification_name;
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  });
+};
+
+/* ***************************
+ * Entregar vista de detalle de vehículo (LA NUEVA)
  * ************************** */
 invCont.getVehicleDetail = async function (req, res, next) {
   const inv_id = req.params.invId;
   const data = await invModel.getInventoryById(inv_id);
 
-  // Usamos la utilidad que ya creaste
   const detailView = await utilities.buildVehicleDetailsHtml(data);
   let nav = await utilities.getNav();
   const vehicleName = `${data.inv_year} ${data.inv_make} ${data.inv_model}`;
