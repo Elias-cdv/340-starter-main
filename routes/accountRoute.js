84c397a235fbf3937753cbe90b38b7d30e8627b1
Elías Cabeza de Vaca <elways38@gmail.com>
2026-03-26 12:38:01 -0500

    fix errors

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
+// Revisa que el nombre sea EXACTO al del archivo en la carpeta utilities
 const regValidate = require("../utilities/account-validation");
-
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build registration view
 router.get(
--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -68,4 +68,40 @@ validate.checkRegData = async (req, res, next) => {
   next();
 };
 
+/* ******************************
+ * Login Data Validation Rules
+ * ***************************** */
+validate.loginRules = () => {
+  return [
+    body("account_email")
+      .trim()
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("A valid email is required."),
+    body("account_password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password cannot be empty."),
+  ];
+};
+
+/* ******************************
+ * Check login data
+ * ***************************** */
+validate.checkLoginData = async (req, res, next) => {
+  const { account_email } = req.body;
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav();
+    res.render("account/login", {
+      errors,
+      title: "Login",
+      nav,
+      account_email,
+    });
+    return;
+  }
+  next();
+};
+
 module.exports = validate;
