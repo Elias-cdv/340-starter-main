ec3cae0ddedfb5c19eb187ad4d71bc7abd05ad46
Elías Cabeza de Vaca <elways38@gmail.com>
2026-03-26 15:51:35 -0500

    call data base

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -36,8 +36,59 @@ async function getInventoryByInvId(inv_id) {
   }
 }
 
+/* *****************************
+ * Task 2: Add New Classification
+ * *************************** */
+async function insertClassification(classification_name) {
+  try {
+    const sql =
+      "INSERT INTO classification (classification_name) VALUES ($1) RETURNING *";
+    return await pool.query(sql, [classification_name]);
+  } catch (error) {
+    return error.message;
+  }
+}
+
+/* *****************************
+ * Task 3: Add New Inventory Item
+ * *************************** */
+async function insertInventory(
+  inv_make,
+  inv_model,
+  inv_year,
+  inv_description,
+  inv_image,
+  inv_thumbnail,
+  inv_price,
+  inv_miles,
+  inv_color,
+  classification_id,
+) {
+  try {
+    const sql =
+      "INSERT INTO inventory (inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *";
+    return await pool.query(sql, [
+      inv_make,
+      inv_model,
+      inv_year,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_miles,
+      inv_color,
+      classification_id,
+    ]);
+  } catch (error) {
+    return error.message;
+  }
+}
+
+// ASEGÚRATE DE EXPORTARLAS. Tu module.exports debe verse algo así:
 module.exports = {
   getClassifications,
   getInventoryByClassificationId,
-  getInventoryByInvId,
+  getInventoryByInventoryId,
+  insertClassification,
+  insertInventory,
 };
