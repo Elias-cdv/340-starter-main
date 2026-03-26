3b763fce1505156bac41d995b419d1dbaea7017b
Elías Cabeza de Vaca <elways38@gmail.com>
2026-03-26 11:47:23 -0500

    fix: registationRules to registrationRules change in account-validation.js and accountRoute.js

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -13,7 +13,7 @@ router.get(
 // Process the registration data
 router.post(
   "/register",
-  regValidate.registationRules(),
+  regValidate.registrationRules(),
   regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount),
 );
