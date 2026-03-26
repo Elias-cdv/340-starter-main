e15b7d911788ef20877dfdc76b2b716aab82bd74
Elías Cabeza de Vaca <elways38@gmail.com>
2026-03-26 11:36:13 -0500

    add route of account validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
-
+const regValidate = require("../utilities/account-validation");
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build registration view
 router.get(
@@ -10,8 +10,11 @@ router.get(
   utilities.handleErrors(accountController.buildRegister),
 );
 
+// Process the registration data
 router.post(
   "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount),
 );
 
