de8f8c757326b227e808eca0fc289471b690b0f2
Elías Cabeza de Vaca <elways38@gmail.com>
2026-04-08 13:41:33 -0500

    fix error 4

--- server.js
+++ server.js
@@ -46,8 +46,7 @@ app.use(static);
 app.get("/", utilities.handleErrors(baseController.buildHome)); // Home primero
 app.use("/inv", inventoryRoute);
 app.use("/account", accountRoute);
-app.use("/review", reviewRoute); // Reseñas aquí
-
+app.use("/review", require("./routes/reviewRoute"));
 /* Error 404 (Al final de todo) */
 app.use(async (req, res, next) => {
   next({ status: 404, message: "Sorry, it seems we lost that page." });
